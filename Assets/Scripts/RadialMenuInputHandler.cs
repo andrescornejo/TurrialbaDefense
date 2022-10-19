9d9cb51d51e5994c0b4916591de40ae17ffe7dd6
Jose Pablo Muñoz <jpmunoz@estudiantec.cr>
2022-10-19 10:46:49 -0600

    Added solid collider to soils

--- Assets/Scripts/RadialMenuInputHandler.cs
+++ Assets/Scripts/RadialMenuInputHandler.cs
@@ -9,6 +9,14 @@ public class RadialMenuInputHandler : MonoBehaviour
     
     public InputActionProperty thumbstick;
     public GameObject selectionIndicator;
+    // Holds the current state of the 4 available crops
+    /*
+        1 = tomato
+        2 = pumpkin
+        3 =
+        4 =
+    */
+    [System.NonSerialized] public int cropState = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -27,25 +35,33 @@ public class RadialMenuInputHandler : MonoBehaviour
             thumbstick.action.ReadValue<Vector2>().x >= -0.5 &&
             thumbstick.action.ReadValue<Vector2>().y >= 0.5)
         {
+            // North position
             selectionIndicator.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            cropState = 1;
         }
         else if (thumbstick.action.ReadValue<Vector2>().x <= 0.5 && 
                  thumbstick.action.ReadValue<Vector2>().x >= -0.5 &&
                  thumbstick.action.ReadValue<Vector2>().y <= -0.5)
         {
+            // South position
             selectionIndicator.transform.localRotation = Quaternion.Euler(0, 0, 180);
+            cropState = 3;
         }
         else if (thumbstick.action.ReadValue<Vector2>().y <= 0.5 && 
                  thumbstick.action.ReadValue<Vector2>().y >= -0.5 &&
                  thumbstick.action.ReadValue<Vector2>().x >= 0.5)
         {
+            // West position
             selectionIndicator.transform.localRotation = Quaternion.Euler(0, 0, 270);
+            cropState = 2;
         }
         else if (thumbstick.action.ReadValue<Vector2>().y <= 0.5 && 
                  thumbstick.action.ReadValue<Vector2>().y >= -0.5 &&
                  thumbstick.action.ReadValue<Vector2>().x <= -0.5)
         {
+            // East position
             selectionIndicator.transform.localRotation = Quaternion.Euler(0, 0, 90);
+            cropState = 4;
         }
     }
 }
