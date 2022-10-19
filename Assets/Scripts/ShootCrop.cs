4476ec92dfec58926d6a668e17486ca877ff0919
Jose Pablo Muñoz <jpmunoz@estudiantec.cr>
2022-10-19 13:55:06 -0600

    Script camelCase refactor

--- Assets/Scripts/ShootCrop.cs
+++ Assets/Scripts/ShootCrop.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class ShootCrop : MonoBehaviour
 {
 
-    private GameObject current_projectile;
+    private GameObject currentProjectile;
     public GameObject radialMenu;
     private RadialMenuInputHandler inputHandler;
     public GameObject ammo1;
 	public GameObject ammo2;
 	public GameObject ammo3;
 	public GameObject ammo4;
-    public GameObject shoot_point;
+    public GameObject origin;
     public float force;
 
     // Start is called before the first frame update
@@ -27,24 +27,24 @@ public class ShootCrop : MonoBehaviour
         switch (inputHandler.cropState)
         {
             case 1:
-                current_projectile = ammo1;
+                currentProjectile = ammo1;
                 break;
 			case 2:
-				current_projectile = ammo2;
+				currentProjectile = ammo2;
 				break;
 			case 3:
-				current_projectile = ammo3;
+				currentProjectile = ammo3;
 				break;
 			case 4:
-				current_projectile = ammo4;
+				currentProjectile = ammo4;
 				break;
         }
     }
 
     public void shoot()
     {
-        GameObject bullet = Instantiate(current_projectile, shoot_point.transform.position, transform.rotation);
-        bullet.GetComponent<Rigidbody>().AddForce(shoot_point.transform.forward * force);
+        GameObject bullet = Instantiate(currentProjectile, origin.transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(origin.transform.forward * force);
         Destroy(bullet, 4);
     }
 }
