3ada5a13b995448417dc2bbdd3da206f54f6ffb8
joseSalazar4 <50027841+joseSalazar4@users.noreply.github.com>
2022-10-22 22:26:32 -0600

    Walking enemy with damage until dead

---
+++ Assets/DetectHit.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;  
+public class DetectHit : MonoBehaviour
+{
+    float health = 100;
+    public TMP_Text mText;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Corn Fruit":
+                health -= 20;
+                break;
+            case "Pumpkin Fruit":
+                health -= 50;
+                break;
+            case "Turnip Fruit":
+                health -= 30;
+                break;
+            case "Tomato Fruit":
+                health -= 40;
+                break;
+            default:
+                return;
+        }
+  
+        if (health < 1) {
+
+            GetComponent<Animator>().Play("Base Layer.die");
+            Destroy(gameObject, 2);
+        }
+        mText.text = health.ToString();
+    }
+
+   
+  
+
+}
