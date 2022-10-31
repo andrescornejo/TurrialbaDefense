dcdfd25e5a34481750a22b21e396c7e24beba275
joseSalazar4 <50027841+joseSalazar4@users.noreply.github.com>
2022-10-30 18:04:52 -0600

    Help messages-new asset-new height to crop-faster times

--- Assets/Scripts/EnemyProperties.cs
+++ Assets/Scripts/EnemyProperties.cs
@@ -9,6 +9,7 @@ public class EnemyProperties : MonoBehaviour
     public TMP_Text healthIndicator;
     private ProjectileProperties projectileProperties;
     public float speed;
+    private bool gotHit;
     [System.NonSerialized] public bool isDead;
 
     // Start is called before the first frame update
@@ -21,25 +22,32 @@ public class EnemyProperties : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isDead) transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+
+        if (!isDead && !gotHit) transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 
     // On collison with a fruit obtains the projectile properties from that fruit
     // Then, destroys the fruit and applies the damage
-    void OnTriggerEnter(Collider collider)
+    IEnumerator OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag.Contains("Fruit"))
         {
+            Debug.Log(health);
             projectileProperties = collider.gameObject.GetComponent<ProjectileProperties>();
             health -= projectileProperties.damage;
+            GetComponent<Animator>().Play("Actions.GetHit");
+            gotHit = true;
+            
             if (health <= 0) {
                 health = 0;
                 isDead = true;
-                GetComponent<Animator>().Play("Base Layer.die");
+                GetComponent<Animator>().Play("Actions.Die");
                 Destroy(gameObject, 2);
             }
             Destroy(collider.gameObject);
             healthIndicator.text = "Health: " + health;
+            yield return new WaitForSeconds(1.67f);
+            gotHit = false;
         } 
     }
 }
